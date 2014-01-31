13589cd9346cd2959c4be6a7cb88693bbf410b99
forman <norman.fomferra@brockmann-consult.de>
2014-01-31 17:55:45 +0100

    Added Lucene Indexer and Retrieval Tool

---
+++ pfa-database/src/main/java/org/esa/pfa/db/DsIndexer.java
@@ -0,0 +1,297 @@
+package org.esa.pfa.db;
+
+import com.bc.ceres.core.Assert;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.FieldInfo;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.NumericUtils;
+import org.apache.lucene.util.Version;
+import org.esa.pfa.fe.op.AttributeType;
+import org.esa.pfa.fe.op.FeatureType;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * The PFA Dataset Indexer Tool.
+ *
+ * @author Norman
+ */
+public class DsIndexer {
+
+    private static FieldType storedIntType;
+    private static FieldType storedLongType;
+    private static FieldType storedFloatType;
+    private static FieldType unstoredFloatType;
+    private static Map<Class<?>, IndexableFieldFactory> indexableFieldFactoryMap = new HashMap<>();
+
+    private IndexWriter indexWriter;
+    private boolean verbose;
+    private long docID;
+    private List<IndexableFieldRiser> indexableFieldRisers;
+
+    static {
+        initIndexableFieldFactories();
+    }
+
+    public static void main(String[] args) {
+        try {
+            new DsIndexer().run(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private interface IndexableFieldFactory {
+        IndexableField createIndexableField(String fieldName, String fieldValue, Field.Store fieldStore);
+    }
+
+    private class IndexableFieldRiser {
+        final String fieldName;
+        final IndexableFieldFactory indexableFieldFactory;
+
+        public IndexableFieldRiser(String fieldName, IndexableFieldFactory indexableFieldFactory) {
+            Assert.notNull(fieldName, "fieldName");
+            Assert.notNull(indexableFieldFactory, "indexableFieldFactory");
+            this.fieldName = fieldName;
+            this.indexableFieldFactory = indexableFieldFactory;
+        }
+
+        public String getFieldName() {
+            return fieldName;
+        }
+
+        public IndexableFieldFactory getIndexableFieldFactory() {
+            return indexableFieldFactory;
+        }
+    }
+
+    private void run(String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new Exception("usage: DsIndexer <dataset-dir>");
+        }
+        String dsPath = args[0];
+        File dsDir = new File(dsPath);
+        DatasetDescriptor dsDescriptor = DatasetDescriptor.read(new File(dsDir, "ds-descriptor.xml"));
+
+
+        indexableFieldRisers = new ArrayList<>();
+        FeatureType[] featureTypes = dsDescriptor.getFeatureTypes();
+        for (FeatureType featureType : featureTypes) {
+            if (featureType.hasAttributes()) {
+                AttributeType[] attributeTypes = featureType.getAttributeTypes();
+                for (AttributeType attributeType : attributeTypes) {
+                    IndexableFieldRiser indexableFieldRiser = getIndexableFieldRiser(featureType.getName() + "." + attributeType.getName(), attributeType);
+                    if (indexableFieldRiser != null) {
+                        indexableFieldRisers.add(indexableFieldRiser);
+                    }
+                }
+            } else {
+                IndexableFieldRiser indexableFieldRiser = getIndexableFieldRiser(featureType.getName(), featureType);
+                if (indexableFieldRiser != null) {
+                    indexableFieldRisers.add(indexableFieldRiser);
+                }
+            }
+        }
+
+
+        Analyzer analyzer = new KeywordAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_46, analyzer);
+        config.setRAMBufferSizeMB(16);
+        if (verbose) {
+            config.setInfoStream(System.out);
+        }
+
+        long t1, t2;
+
+        try (Directory indexDirectory = FSDirectory.open(new File(dsPath, "lucene-index"))) {
+            indexWriter = new IndexWriter(indexDirectory, config);
+            try {
+                t1 = System.currentTimeMillis();
+                processDsDir(dsDir);
+                t2 = System.currentTimeMillis();
+            } finally {
+                indexWriter.close();
+            }
+        }
+
+        System.out.println(docID + "(s) patches added to index within " + ((t2 - t1) / 1000) + " seconds");
+
+    }
+
+    private void processDsDir(File dsDir) throws Exception {
+        File[] fexDirs = dsDir.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory() && file.getName().endsWith(".fex");
+            }
+        });
+
+        if (fexDirs == null || fexDirs.length == 0) {
+            throw new Exception("empty dataset directory: " + dsDir);
+        }
+
+        for (File fexDir : fexDirs) {
+            processFexDir(fexDir);
+        }
+    }
+
+    private boolean processFexDir(File fexDir) {
+        File[] patchDirs = fexDir.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory() && file.getName().startsWith("x");
+            }
+        });
+
+        if (patchDirs == null || patchDirs.length == 0) {
+            System.out.println("warning: no patches in fex directory: " + fexDir);
+            return false;
+        }
+
+        for (File patchDir : patchDirs) {
+            processPatchDir(patchDir);
+        }
+
+        return true;
+    }
+
+    private boolean processPatchDir(File patchDir) {
+        File featureFile = new File(patchDir, "features.txt");
+        if (!featureFile.exists()) {
+            System.out.println("warning: missing features: " + featureFile);
+            return false;
+        }
+
+        String name = patchDir.getName();
+        int patchX = Integer.parseInt(name.substring(1, 3));
+        int patchY = Integer.parseInt(name.substring(4, 6));
+
+        String fexDirName = patchDir.getParentFile().getName();
+        String productName = fexDirName.substring(0, fexDirName.length() - 4);
+
+        try {
+            addPatchToIndex(productName, patchX, patchY, featureFile);
+        } catch (IOException e) {
+            System.out.printf("i/o exception: %s: file: %s%n", e.getMessage(), featureFile);
+            return false;
+        }
+        return true;
+    }
+
+    private void addPatchToIndex(String productName, int patchX, int patchY, File featureFile) throws IOException {
+
+        Document doc = new Document();
+        // ID value is stored in the index.
+        doc.add(new LongField("id", docID, storedLongType));
+        doc.add(new TextField("product", productName, Field.Store.YES));
+        doc.add(new IntField("px", patchX, storedIntType));
+        doc.add(new IntField("py", patchY, storedIntType));
+
+        Properties featureValues = new Properties();
+        try (FileReader reader = new FileReader(featureFile)) {
+            featureValues.load(reader);
+        }
+
+        for (IndexableFieldRiser indexableFieldRiser : indexableFieldRisers) {
+            String fieldValue = featureValues.getProperty(indexableFieldRiser.getFieldName());
+            if (fieldValue != null) {
+                IndexableFieldFactory indexableFieldFactory = indexableFieldRiser.getIndexableFieldFactory();
+                doc.add(indexableFieldFactory.createIndexableField(indexableFieldRiser.getFieldName(), fieldValue, Field.Store.YES));
+            }
+        }
+
+        indexWriter.addDocument(doc);
+        System.out.printf("added to index: id=%d, product=%s, px=%d, py=%d\n", docID, productName, patchX, patchY);
+        docID++;
+    }
+
+
+    private IndexableFieldRiser getIndexableFieldRiser(String fieldName, AttributeType attributeType) {
+        Class<?> valueType = attributeType.getValueType();
+        IndexableFieldFactory indexableFieldFactory = indexableFieldFactoryMap.get(valueType);
+        if (indexableFieldFactory == null) {
+            return null;
+        }
+        return new IndexableFieldRiser(fieldName, indexableFieldFactory);
+    }
+
+
+    private static void initIndexableFieldFactories() {
+        storedIntType = createNumericFieldType(FieldType.NumericType.INT, Field.Store.YES, NumericUtils.PRECISION_STEP_DEFAULT);
+        storedLongType = createNumericFieldType(FieldType.NumericType.LONG, Field.Store.YES, NumericUtils.PRECISION_STEP_DEFAULT);
+        storedFloatType = createNumericFieldType(FieldType.NumericType.FLOAT, Field.Store.YES, NumericUtils.PRECISION_STEP_DEFAULT);
+        unstoredFloatType = createNumericFieldType(FieldType.NumericType.FLOAT, Field.Store.NO, NumericUtils.PRECISION_STEP_DEFAULT);
+
+        IndexableFieldFactory intIndexableFieldFactory = new IndexableFieldFactory() {
+            @Override
+            public IndexableField createIndexableField(String fieldName, String fieldValue, Field.Store fieldStore) {
+                int value = Integer.parseInt(fieldValue);
+                return new IntField(fieldName, value, fieldStore);
+            }
+        };
+        indexableFieldFactoryMap.put(Byte.TYPE, intIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Byte.class, intIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Short.TYPE, intIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Short.class, intIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Integer.TYPE, intIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Integer.class, intIndexableFieldFactory);
+
+        IndexableFieldFactory longIndexableFieldFactory = new IndexableFieldFactory() {
+            @Override
+            public IndexableField createIndexableField(String fieldName, String fieldValue, Field.Store fieldStore) {
+                long value = Long.parseLong(fieldValue);
+                return new LongField(fieldName, value, fieldStore);
+            }
+        };
+        indexableFieldFactoryMap.put(Long.TYPE, longIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Long.class, longIndexableFieldFactory);
+
+        IndexableFieldFactory floatIndexableFieldFactory = new IndexableFieldFactory() {
+            @Override
+            public IndexableField createIndexableField(String fieldName, String fieldValue, Field.Store fieldStore) {
+                float value = (float) Double.parseDouble(fieldValue);
+                //System.out.println(">> " + fieldName + " = " + value);
+                return new FloatField(fieldName, value, fieldStore == Field.Store.YES ? storedFloatType : unstoredFloatType);
+            }
+        };
+        indexableFieldFactoryMap.put(Float.TYPE, floatIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Float.class, floatIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Double.TYPE, floatIndexableFieldFactory);
+        indexableFieldFactoryMap.put(Double.class, floatIndexableFieldFactory);
+
+        IndexableFieldFactory stringIndexableFieldFactory = new IndexableFieldFactory() {
+            @Override
+            public IndexableField createIndexableField(String fieldName, String fieldValue, Field.Store fieldStore) {
+                return new StringField(fieldName, fieldValue, fieldStore);
+            }
+        };
+        indexableFieldFactoryMap.put(String.class, stringIndexableFieldFactory);
+        indexableFieldFactoryMap.put(File.class, stringIndexableFieldFactory);
+    }
+
+    /**
+     * Allow to create numeric fields with variable precision step. All other field properties are defaults (see Lucene code).
+     */
+    private static FieldType createNumericFieldType(FieldType.NumericType numericType, Field.Store store, int precisionStep) {
+        final FieldType type = new FieldType();
+        type.setIndexed(true);
+        type.setTokenized(true);
+        type.setOmitNorms(true);
+        type.setIndexOptions(FieldInfo.IndexOptions.DOCS_ONLY);
+        type.setNumericType(numericType);
+        type.setStored(store == Field.Store.YES);
+        type.setNumericPrecisionStep(precisionStep);
+        type.freeze();
+        return type;
+    }
+}
---
+++ pfa-database/src/main/java/org/esa/pfa/db/DsSearcher.java
@@ -0,0 +1,206 @@
+package org.esa.pfa.db;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
+import org.apache.lucene.queryparser.flexible.standard.config.NumericConfig;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.MMapDirectory;
+import org.esa.pfa.fe.op.AttributeType;
+import org.esa.pfa.fe.op.FeatureType;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+/**
+ * The PFA Dataset Indexer Tool.
+ *
+ * @author Norman
+ */
+public class DsSearcher {
+
+    static NumericConfig intNumericConfig = new NumericConfig(8, NumberFormat.getNumberInstance(Locale.ENGLISH), FieldType.NumericType.INT);
+    static NumericConfig longNumericConfig = new NumericConfig(8, NumberFormat.getNumberInstance(Locale.ENGLISH), FieldType.NumericType.LONG);
+    static NumericConfig floatNumericConfig = new NumericConfig(8, NumberFormat.getNumberInstance(Locale.ENGLISH), FieldType.NumericType.FLOAT);
+    static Map<Class<?>, NumericConfig> numericConfigMap;
+
+    int threadCount = 1;
+    int maxHitCount = 100;
+    DatasetDescriptor dsDescriptor;
+
+    public static void main(String[] args) {
+        try {
+            new DsSearcher().run(args);
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void run(String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new Exception("usage: DsSearcher <dataset-dir>");
+        }
+
+        String dsPath = args[0];
+        File dsDir = new File(dsPath);
+        dsDescriptor = DatasetDescriptor.read(new File(dsDir, "ds-descriptor.xml"));
+
+        StandardQueryParser parser = new StandardQueryParser();
+        parser.setNumericConfigMap(getNumericConfigMap(dsDescriptor));
+
+        try (Directory indexDirectory = new MMapDirectory(new File(dsDir, "lucene-index"))) {
+            try (IndexReader indexReader = DirectoryReader.open(indexDirectory)) {
+                IndexSearcher indexSearcher = new IndexSearcher(indexReader, Executors.newFixedThreadPool(this.threadCount));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String queryExpr;
+                do {
+                    System.out.print(">>> ");
+                    System.out.flush();
+                    queryExpr = reader.readLine();
+                } while (queryExpr != null && processQuery(indexSearcher, parser, queryExpr));
+            }
+        }
+
+    }
+
+    private boolean processQuery(IndexSearcher indexSearcher, StandardQueryParser parser, String queryExpr) {
+
+        queryExpr = queryExpr.trim();
+
+        if (queryExpr.isEmpty()) {
+            return true;
+        }
+
+        if (queryExpr.equalsIgnoreCase("help")) {
+            printHelp();
+            return true;
+        }
+
+        if (queryExpr.equalsIgnoreCase("exit") || queryExpr.equalsIgnoreCase("quit")) {
+            return false;
+        }
+
+        try {
+            Query query = parser.parse(queryExpr, "product");
+
+            long t1 = System.currentTimeMillis();
+            TopDocs topDocs = indexSearcher.search(query, null, this.maxHitCount);
+            long t2 = System.currentTimeMillis();
+
+            if (topDocs.totalHits == 0) {
+                System.out.println("no documents found within " + (t2 - t1) + " ms");
+            } else {
+                System.out.println("found " + topDocs.totalHits + " documents(s) within " + (t2 - t1) + " ms:");
+                ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+                for (int i = 0; i < scoreDocs.length; i++) {
+                    ScoreDoc scoreDoc = scoreDocs[i];
+                    int docID = scoreDoc.doc;
+                    Document doc = indexSearcher.doc(docID);
+                    String productName = doc.getValues("product")[0];
+                    String patchX = doc.getValues("px")[0];
+                    String patchY = doc.getValues("py")[0];
+                    System.out.printf("[%3d]: product = %s, px = %s, py = %s%n", i + 1, productName, patchX, patchY);
+                }
+            }
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+
+        return true;
+    }
+
+    private void printHelp() {
+        System.out.println("Searchable Fields:");
+        printAttrHelp("product", String.class, "EO data product name");
+        printAttrHelp("px", Integer.TYPE, "Patch x-coordinate");
+        printAttrHelp("py", Integer.TYPE, "Patch y-coordinate");
+        for (FeatureType featureType : dsDescriptor.getFeatureTypes()) {
+            if (featureType.hasAttributes()) {
+                AttributeType[] attributeTypes = featureType.getAttributeTypes();
+                for (AttributeType attributeType : attributeTypes) {
+                        printAttrHelp(featureType.getName() + "." + attributeType.getName(), attributeType);
+                }
+            } else {
+                printAttrHelp(featureType.getName(), featureType);
+            }
+        }
+        System.out.println();
+        System.out.println("Query Parser Syntax: https://lucene.apache.org/core/4_6_0/queryparser/org/apache/lucene/queryparser/classic/package-summary.html#package_description");
+        System.out.println();
+    }
+
+    private void printAttrHelp(String fieldName, AttributeType attributeType) {
+        printAttrHelp(fieldName,attributeType.getValueType(), attributeType.getDescription());
+    }
+
+    private void printAttrHelp(String fieldName, Class<?> valueType, String description) {
+        System.out.printf("  %s: %s  --  %s\n", fieldName, valueType, description);
+    }
+
+    private Map<String, NumericConfig> getNumericConfigMap(DatasetDescriptor dsDescriptor) {
+        Map<String, NumericConfig> numericConfigMap = new HashMap<>();
+        FeatureType[] featureTypes = dsDescriptor.getFeatureTypes();
+        for (FeatureType featureType : featureTypes) {
+            if (featureType.hasAttributes()) {
+                AttributeType[] attributeTypes = featureType.getAttributeTypes();
+                for (AttributeType attributeType : attributeTypes) {
+
+                    NumericConfig numericConfig = getNumericConfig(attributeType);
+                    if (numericConfig != null) {
+                        numericConfigMap.put(featureType.getName() + "." + attributeType.getName(), numericConfig);
+                    }
+                }
+            } else {
+                NumericConfig numericConfig = getNumericConfig(featureType);
+                if (numericConfig != null) {
+                    numericConfigMap.put(featureType.getName(), numericConfig);
+                }
+            }
+        }
+        return numericConfigMap;
+    }
+
+    private static NumericConfig getNumericConfig(AttributeType attributeType) {
+        Class<?> valueType = attributeType.getValueType();
+        return numericConfigMap.get(valueType);
+    }
+
+
+    static {
+        numericConfigMap = new HashMap<>();
+
+        numericConfigMap.put(Byte.TYPE, intNumericConfig);
+        numericConfigMap.put(Byte.class, intNumericConfig);
+        numericConfigMap.put(Short.TYPE, intNumericConfig);
+        numericConfigMap.put(Short.class, intNumericConfig);
+        numericConfigMap.put(Integer.TYPE, intNumericConfig);
+        numericConfigMap.put(Integer.class, intNumericConfig);
+
+        numericConfigMap.put(Long.TYPE, longNumericConfig);
+        numericConfigMap.put(Long.class, longNumericConfig);
+
+        numericConfigMap.put(Float.TYPE, floatNumericConfig);
+        numericConfigMap.put(Float.class, floatNumericConfig);
+        numericConfigMap.put(Double.TYPE, floatNumericConfig);
+        numericConfigMap.put(Double.class, floatNumericConfig);
+    }
+
+
+
+}
