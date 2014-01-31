fcbb168b7cd837c66b4440d86c4a799962ebcd9c
forman <norman.fomferra@brockmann-consult.de>
2014-01-31 10:10:10 +0100

    New modular project structure

---
+++ pfa-core/src/main/java/org/esa/pfa/fe/op/Patch.java
@@ -0,0 +1,46 @@
+package org.esa.pfa.fe.op;
+
+import org.esa.beam.framework.datamodel.Product;
+
+import java.awt.*;
+
+/**
+ * @author Norman Fomferra
+ */
+public final class Patch {
+
+
+    private final int patchX;
+    private final int patchY;
+    private final Rectangle patchRegion;
+    private final Product patchProduct;
+    private final String patchName;
+
+    public Patch(int patchX, int patchY, Rectangle patchRegion, Product patchProduct) {
+        this.patchX = patchX;
+        this.patchY = patchY;
+        this.patchName = String.format("x%02dy%02d", patchX, patchY);
+        this.patchRegion = patchRegion;
+        this.patchProduct = patchProduct;
+    }
+
+    public String getPatchName() {
+        return patchName;
+    }
+
+    public int getPatchX() {
+        return patchX;
+    }
+
+    public int getPatchY() {
+        return patchY;
+    }
+
+    public Rectangle getPatchRegion() {
+        return patchRegion;
+    }
+
+    public Product getPatchProduct() {
+        return patchProduct;
+    }
+}
---
+++ pfa-core/src/main/java/org/esa/pfa/fe/op/out/DefaultPatchWriterFactory.java
@@ -0,0 +1,70 @@
+package org.esa.pfa.fe.op.out;
+
+import com.bc.ceres.core.ExtensionFactory;
+import com.bc.ceres.core.ExtensionManager;
+import org.esa.beam.framework.dataio.ProductIO;
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.util.logging.BeamLogManager;
+import org.esa.pfa.fe.op.Feature;
+import org.esa.pfa.fe.op.Patch;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Norman Fomferra
+ */
+public class DefaultPatchWriterFactory extends PatchWriterFactory {
+
+    static {
+        ExtensionManager.getInstance().register(Feature.class, new FeatureOutputFactory());
+    }
+
+    @Override
+    public PatchWriter createFeatureOutput(Product sourceProduct) throws IOException {
+        return new DefaultPatchWriter(this, sourceProduct);
+    }
+
+    private static class FeatureOutputFactory implements ExtensionFactory {
+        @Override
+        public FeatureOutput getExtension(Object object, Class<?> extensionType) {
+            Feature feature = (Feature) object;
+            Object value = feature.getValue();
+            if (value instanceof Product) {
+                return new ProductFeatureOutput();
+            } else if (value instanceof RenderedImage) {
+                return new RenderedImageFeatureOutput();
+            }
+            return null;
+        }
+
+        @Override
+        public Class<?>[] getExtensionTypes() {
+            return new Class<?>[]{FeatureOutput.class};
+        }
+
+        private static class ProductFeatureOutput implements FeatureOutput {
+            @Override
+            public String writeFeature(Patch patch, Feature feature, String dirPath) throws IOException {
+                Product patchProduct = (Product) feature.getValue();
+                String path = new File(dirPath, feature.getName() + ".dim").getPath();
+                BeamLogManager.getSystemLogger().info("Writing " + path);
+                ProductIO.writeProduct(patchProduct, path, "BEAM-DIMAP");
+                return path;
+            }
+        }
+
+        private static class RenderedImageFeatureOutput implements FeatureOutput {
+            @Override
+            public String writeFeature(Patch patch, Feature feature, String dirPath) throws IOException {
+                RenderedImage image = (RenderedImage) feature.getValue();
+                File output = new File(dirPath, feature.getName() + ".png");
+                BeamLogManager.getSystemLogger().info("Writing " + output);
+                ImageIO.write(image, "PNG", output);
+                return output.getPath();
+            }
+        }
+    }
+}
