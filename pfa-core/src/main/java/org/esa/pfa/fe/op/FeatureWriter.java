fd0e6157a58d350d49d6586263d1becc8b9e9edf
Luis Veci <lveci@array.ca>
2014-02-12 00:07:15 -0500

    update patch definition

--- pfa-core/src/main/java/org/esa/pfa/fe/op/FeatureWriter.java
+++ pfa-core/src/main/java/org/esa/pfa/fe/op/FeatureWriter.java
@@ -220,7 +220,7 @@ public abstract class FeatureWriter extends Operator implements Output {
         }
     }
 
-    private static Product createSubset(Product sourceProduct, Rectangle subsetRegion) throws IOException {
+    public static Product createSubset(Product sourceProduct, Rectangle subsetRegion) throws IOException {
         final ProductSubsetDef subsetDef = new ProductSubsetDef();
         subsetDef.addNodeNames(sourceProduct.getTiePointGridNames());
         subsetDef.addNodeNames(sourceProduct.getBandNames());
--- pfa-core/src/main/java/org/esa/pfa/fe/op/Patch.java
+++ pfa-core/src/main/java/org/esa/pfa/fe/op/Patch.java
@@ -3,25 +3,45 @@ package org.esa.pfa.fe.op;
 import org.esa.beam.framework.datamodel.Product;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Norman Fomferra
  */
 public final class Patch {
 
-
     private final int patchX;
     private final int patchY;
     private final Rectangle patchRegion;
     private final Product patchProduct;
     private final String patchName;
+    private final List<Feature> featureList = new ArrayList<Feature>(10);
+
+    private final int uid;
+    private static int uidCnt = 0;
+
+    private int label = -1;
+    private double confidence;
+
+    private BufferedImage image = null;
 
     public Patch(int patchX, int patchY, Rectangle patchRegion, Product patchProduct) {
+        uid = createUniqueID();
         this.patchX = patchX;
         this.patchY = patchY;
         this.patchName = String.format("x%02dy%02d", patchX, patchY);
         this.patchRegion = patchRegion;
         this.patchProduct = patchProduct;
+    }
+
+    private synchronized int createUniqueID() {
+        return uidCnt++;
+    }
+
+    public int getID() {
+        return uid;
     }
 
     public String getPatchName() {
@@ -42,5 +62,37 @@ public final class Patch {
 
     public Product getPatchProduct() {
         return patchProduct;
+    }
+
+    public void setImage(BufferedImage img) {
+        image = img;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void addFeature(final Feature fea) {
+        featureList.add(fea);
+    }
+
+    public Feature[] getFeatures() {
+        return featureList.toArray(new Feature[featureList.size()]);
+    }
+
+    public void setConfidence(final double confidence) {
+        this.confidence = confidence;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+
+    public void setLabel(final int label) {
+        this.label = label;
+    }
+
+    public int getLabel() {
+        return label;
     }
 }
