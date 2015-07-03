bf815be52e2aa909190497eac21a34d229842dba
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2015-07-03 16:22:10 +0200

    let server running

--- pfa-ws-server/src/main/java/org/esa/pfa/ws/ServerMain.java
+++ pfa-ws-server/src/main/java/org/esa/pfa/ws/ServerMain.java
@@ -62,8 +62,8 @@ public class ServerMain {
         final HttpServer server = startServer(serverUri);
         System.out.println(String.format("Jersey app started with WADL available at " +
                                                  "%s/application.wadl\nHit enter to stop it...", serverUri));
-        System.in.read();
-        server.shutdownNow();
+//        System.in.read();
+//        server.shutdownNow();
     }
 }
 
