8d0a55bc382498655f515bd54f2c81fdcb809b0d
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-07-20 14:12:18 +0200

    fix

--- pfa-bitemp-fex/src/main/java/org/esa/pfa/fe/spectral/BiTempPreprocessor.java
+++ pfa-bitemp-fex/src/main/java/org/esa/pfa/fe/spectral/BiTempPreprocessor.java
@@ -2,24 +2,30 @@ package org.esa.pfa.fe.spectral;
 
 import com.bc.ceres.core.Assert;
 import com.bc.ceres.glevel.MultiLevelImage;
+import com.sun.media.jai.util.SunTileCache;
 import org.esa.snap.framework.dataio.ProductIO;
 import org.esa.snap.framework.datamodel.Band;
 import org.esa.snap.framework.datamodel.GeoCoding;
 import org.esa.snap.framework.datamodel.GeoPos;
+import org.esa.snap.framework.datamodel.Mask;
 import org.esa.snap.framework.datamodel.MetadataElement;
 import org.esa.snap.framework.datamodel.PixelPos;
 import org.esa.snap.framework.datamodel.Product;
 import org.esa.snap.framework.datamodel.ProductData;
+import org.esa.snap.framework.datamodel.RasterDataNode;
 import org.esa.snap.framework.gpf.OperatorException;
 import org.esa.snap.gpf.operators.standard.SubsetOp;
 import org.esa.snap.util.SystemUtils;
 
+import javax.media.jai.JAI;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -34,17 +40,31 @@ import java.util.Locale;
 public class BiTempPreprocessor {
 
     public static final DateFormat DATE_FORMAT = ProductData.UTC.createDateFormat("yyyyMMddHHmm");
+    public static final int PATCH_SIZE = 200;
+    public static final double PIXEL_RESOLUTION = 0.009;
 
     private PatchCS patchCS;
     private Path targetDir;
 
     // Processing time stats
-    private long patchCount;
+    private long writtenPatchCount;
     private double patchTimeSum;
-    private long productCount;
+    private long processedProductCount;
     private double productTimeSum;
+    private long recordId;
 
-    public BiTempPreprocessor() {
+    private PrintWriter printWriter;
+
+    public BiTempPreprocessor() throws IOException {
+        printWriter = new PrintWriter(new FileWriter("BiTempPreprocessor.csv"));
+        printWriter.printf("id\t#products\t#patches\ttime/patch(ms)\tcached(MiB)\tfree(MiB)\ttotal(MiB)\tmax(MiB)%n");
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            @Override
+            public void run() {
+                printWriter.close();
+            }
+        });
     }
 
     void process(Product product) throws IOException {
@@ -86,7 +106,7 @@ public class BiTempPreprocessor {
                 subsetOp.setCopyMetadata(false);
 
                 Product subsetProduct = subsetOp.getTargetProduct();
-                ProductData.UTC startTime = subsetProduct.getStartTime();
+                ProductData.UTC startTime = reprojectedProduct.getStartTime();
 
                 String patchIdX = String.format(PatchCS.PATCH_X_FORMAT, patchX);
                 String patchIdY = String.format(PatchCS.PATCH_Y_FORMAT, patchY);
@@ -109,11 +129,29 @@ public class BiTempPreprocessor {
                     validPixelRatio = MaskStats.maskedRatio(validMaskImage);
                 }
 
-                if (validPixelRatio > 0.2) {
-                    writePatchProduct(subsetProduct, patchIdX, patchIdY);
-                } else {
-                    System.out.printf("Rejected patch %s with valid-pixel ratio of %.1f%%\n", subsetProduct.getName(), 100 * validPixelRatio);
+                try {
+                    if (validPixelRatio > 0.2) {
+                        writePatchProduct(subsetProduct, patchIdX, patchIdY);
+                    } else {
+                        System.out.printf("Rejected patch %s with valid-pixel ratio of %.1f%%\n", subsetProduct.getName(), 100 * validPixelRatio);
+                    }
+                } finally {
+                    disposeImages(subsetProduct.getBands());
+                    disposeImages(subsetProduct.getTiePointGrids());
+                    disposeImages(subsetProduct.getMaskGroup().toArray(new Mask[0]));
+                    subsetProduct.dispose();
                 }
+
+                printWriter.printf("%d\t%d\t%d\t%.1f\t%d\t%d\t%d\t%d%n",
+                                   recordId,
+                                   processedProductCount,
+                                   writtenPatchCount,
+                                   writtenPatchCount > 0 ? patchTimeSum / writtenPatchCount : 0.0,
+                                   mibs(((SunTileCache) JAI.getDefaultInstance().getTileCache()).getCacheMemoryUsed()),
+                                   mibs(Runtime.getRuntime().freeMemory()),
+                                   mibs(Runtime.getRuntime().totalMemory()),
+                                   mibs(Runtime.getRuntime().maxMemory()));
+                recordId++;
 
                 patchX++;
             }
@@ -121,9 +159,28 @@ public class BiTempPreprocessor {
         }
     }
 
-    private void writePatchProduct(Product subsetProduct, String patchIdX, String patchIdY) throws IOException {
+    private void disposeImages(RasterDataNode[] rasters) {
+        for (RasterDataNode raster : rasters) {
+            MultiLevelImage sourceImage = raster.getSourceImage();
+            if (sourceImage != null) {
+                sourceImage.dispose();
+                raster.setSourceImage(null);
+            }
+            MultiLevelImage validMaskImage = raster.getValidMaskImage();
+            if (validMaskImage != null) {
+                validMaskImage.dispose();
+                raster.resetValidMask();
+            }
+        }
+    }
+
+    private long mibs(long mem) {
+        return Math.round(mem / 1024. / 1024.);
+    }
+
+    private void writePatchProduct(Product patchProduct, String patchIdX, String patchIdY) throws IOException {
         Path outputDir = targetDir.resolve(patchIdX).resolve(patchIdY);
-        Path outputFile = outputDir.resolve(subsetProduct.getName() + ".dim");
+        Path outputFile = outputDir.resolve(patchProduct.getName() + ".dim");
 
         if (!Files.exists(outputDir)) {
             Files.createDirectories(outputDir);
@@ -133,23 +190,21 @@ public class BiTempPreprocessor {
         long dt;
         try {
             long t1 = System.currentTimeMillis();
-            ProductIO.writeProduct(subsetProduct, outputFile.toFile(), "BEAM-DIMAP", false);
+            ProductIO.writeProduct(patchProduct, outputFile.toFile(), "BEAM-DIMAP", false);
             long t2 = System.currentTimeMillis();
             dt = t2 - t1;
-            patchCount++;
+            writtenPatchCount++;
             patchTimeSum += dt;
-            System.out.printf("Patch written after %d ms (%.1f ms in average)\n", dt, patchTimeSum / patchCount);
+            System.out.printf("Patch written after %d ms (%.1f ms in average)\n", dt, patchTimeSum / writtenPatchCount);
         } catch (IOException e) {
             System.err.println("ERROR: " + e.getMessage());
-        } finally {
-            subsetProduct.dispose();
         }
     }
 
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.ENGLISH);
-        System.setProperty("snap.dataio.reader.tileWidth", "*");
-        System.setProperty("snap.dataio.reader.tileHeight", "200");
+        System.setProperty("snap.dataio.reader.tileWidth", (PATCH_SIZE * 2) + "");
+        System.setProperty("snap.dataio.reader.tileHeight", (PATCH_SIZE * 2) + "");
         SystemUtils.init3rdPartyLibs(BiTempPreprocessor.class);
 
         new BiTempPreprocessor().run(args);
@@ -160,7 +215,7 @@ public class BiTempPreprocessor {
             System.out.println("Usage: " + getClass().getSimpleName() + " <target-dir> <source> [<source> ...]");
             System.exit(1);
         }
-        patchCS = new PatchCS(200, 0.009);
+        patchCS = new PatchCS(PATCH_SIZE, PIXEL_RESOLUTION);
         FileFilter fileFilter = file -> {
             String name = file.getName();
             return name.startsWith("MER_RR__1P") && name.endsWith(".N1");
@@ -194,8 +249,8 @@ public class BiTempPreprocessor {
             long t2 = System.currentTimeMillis();
             double dt = (t2 - t1) / 1000.0;
             productTimeSum += dt;
-            productCount++;
-            System.out.printf("Source product processed after %.2f s (%.2f in average)\n", dt, productTimeSum / productCount);
+            processedProductCount++;
+            System.out.printf("Source product processed after %.2f s (%.2f in average)\n", dt, productTimeSum / processedProductCount);
         } catch (IOException e) {
             System.err.println("ERROR: " + e.getMessage());
         }
