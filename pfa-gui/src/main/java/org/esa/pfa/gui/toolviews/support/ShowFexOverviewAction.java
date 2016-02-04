73a4f433f7b5a081c10e32820c976381adb72ccf
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2016-02-04 10:43:46 +0100

    fix URL browsing on unsupported Desktop Environments

--- pfa-gui/src/main/java/org/esa/pfa/gui/toolviews/support/ShowFexOverviewAction.java
+++ pfa-gui/src/main/java/org/esa/pfa/gui/toolviews/support/ShowFexOverviewAction.java
@@ -1,5 +1,6 @@
 package org.esa.pfa.gui.toolviews.support;
 
+import org.esa.snap.core.util.SystemUtils;
 import org.esa.snap.rcp.util.Dialogs;
 
 import javax.swing.AbstractAction;
@@ -21,11 +22,17 @@ class ShowFexOverviewAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            Desktop.getDesktop().browse(fexOverviewUri);
-        } catch (Throwable t) {
-            Dialogs.showError("Failed to open URL:\n" + fexOverviewUri + ":\n" + t.getMessage());
+        boolean desktopSupported = Desktop.isDesktopSupported();
+        boolean browseSupported = Desktop.getDesktop().isSupported(Desktop.Action.BROWSE);
+        if (desktopSupported && browseSupported) {
+            try {
+                Desktop.getDesktop().browse(fexOverviewUri);
+            } catch (Throwable t) {
+                Dialogs.showError(String.format("Failed to open URL:\n%s:\n%s", fexOverviewUri, t.getMessage()));
+            }
+        } else {
+            SystemUtils.copyToClipboard(fexOverviewUri.toString());
+            Dialogs.showInformation("The URL has been copied to your Clipboard\n");
         }
     }
-
 }
